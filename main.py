17615e795d8db01c074ff57843a17d6c99d39f4b
Bhuvnesh Jain <82877515+Bhuvneshjai@users.noreply.github.com>
2023-08-20 21:46:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,64 @@
 import requests
-import os
 from datetime import datetime
 
-user_api = 'a889a5a6c151cf0282a5335e8d4d688e'
-location = input("Enter City Name : ")
+def fetch_data_from_api():
+    api_key = 'b6907d289e10d714a6e88b30761fae22'  # Given API Key
+    location = 'London,us'  # Given Location
+    complete_api_link = f"https://samples.openweathermap.org/data/2.5/forecast/hourly?q={location}&appid={api_key}"
+    
+    api_link = requests.get(complete_api_link)
+    return api_link.json()
 
-complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
-api_link = requests.get(complete_api_link)
-api_data = api_link.json()
+def get_weather_data(api_data, date_time, attribute):
+    for entry in api_data['list']:
+        if entry['dt_txt'] == date_time:
+            if attribute == 'temp':
+                return entry['main']['temp'] - 273.15  # Convert Kelvin to Celsius
+            elif attribute == 'wind':
+                return entry['wind']['speed']
+            elif attribute == 'pressure':
+                return entry['main']['pressure']
+    return None
 
-if api_data['cod'] == '404':
-    print("Invalid City : {} Please check your city name ".format(location))
-else:
-    temp_city = ((api_data['main']['temp']) -273.15)
-    weather_desc = api_data['weather'][0]['description']
-    hmdt = api_data['main']['humidity']
-    wind_spd = api_data['wind']['speed']
-    date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+def main():
+    api_data = fetch_data_from_api()
+    
+    while True:
+        print("\nOptions:")
+        print("1. Get Temperature")
+        print("2. Get Wind Speed")
+        print("3. Get Pressure")
+        print("0. Exit")
+        
+        choice = input("\nEnter your choice: ")
 
-    print("Weather Status For - {} || {}".format(location.upper(), date_time))
-    print("Current Temperature : {:.2f} deg C".format(temp_city))
-    print("Current Weather Desc : ",weather_desc)
-    print("Current Humidity : ",hmdt,"%")
-    print("Current Wind Speed : ",wind_spd,"KMPH")
+        if choice == '0':
+            break
+        elif choice in ['1', '2', '3']:
+            date_time = input("Enter the date and time in the format 'YYYY-MM-DD HH:00:00': ")
+
+            if choice == '1':
+                temp = get_weather_data(api_data, date_time, 'temp')
+                if temp:
+                    print(f"Temperature at {date_time}: {temp:.2f} deg C")
+                else:
+                    print(f"No data available for {date_time}")
+
+            elif choice == '2':
+                wind_speed = get_weather_data(api_data, date_time, 'wind')
+                if wind_speed:
+                    print(f"Wind Speed at {date_time}: {wind_speed} KMPH")
+                else:
+                    print(f"No data available for {date_time}")
+
+            elif choice == '3':
+                pressure = get_weather_data(api_data, date_time, 'pressure')
+                if pressure:
+                    print(f"Pressure at {date_time}: {pressure} hPa")
+                else:
+                    print(f"No data available for {date_time}")
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
